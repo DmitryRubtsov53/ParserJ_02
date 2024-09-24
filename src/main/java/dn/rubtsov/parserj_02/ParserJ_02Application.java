e9874d662468b583872b92999507f9b7fe7c1d89
DmitryRubtsov53 <2578390@mailru>
2024-09-24 09:56:42 +0500

    Add create table, select data from DB.

--- src/main/java/dn/rubtsov/parserj_02/ParserJ_02Application.java
+++ src/main/java/dn/rubtsov/parserj_02/ParserJ_02Application.java
@@ -1,15 +1,12 @@
 package dn.rubtsov.parserj_02;
 
-import dn.rubtsov.parserj_02.data.Registers;
-
-import dn.rubtsov.parserj_02.processor.DBUtils;
 import dn.rubtsov.parserj_02.processor.ParserJson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import java.util.List;
+
 @SpringBootApplication
 @EnableConfigurationProperties
 public class ParserJ_02Application implements CommandLineRunner {
@@ -26,14 +23,6 @@ public class ParserJ_02Application implements CommandLineRunner {
     }
     @Override
     public void run(String... args) {
-
-        List<Registers> data = parserJson.parseJsonFiles();
-        // Запись данных в БД
-        DBUtils.insertRecord(data);
-        //Вывод в консоль этих же данных
-        data.forEach(e -> System.out.println("registerType: " + e.getRegisterType()
-        + ", " + "restIn: " + e.getRestIn()));
-
     }
 
 }
--- src/test/java/dn/rubtsov/parserj_02/processor/ParserJsonTest.java
+++ src/test/java/dn/rubtsov/parserj_02/processor/ParserJsonTest.java
@@ -1,46 +1,22 @@
 package dn.rubtsov.parserj_02.processor;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import dn.rubtsov.parserj_02.data.Registers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ParserJsonTest {
-@Autowired
-ParserJson parserJson;
-
-    @Test
-    void testOfParseJsonFiles() {
-        List<Registers> data = parserJson.parseJsonFiles();
-        // Проверяем, что данные были корректно считаны
-        assertNotNull(data, "List should not be null");
-        assertFalse(data.isEmpty(), "List should not be empty");
-        assertEquals(3, data.size(),"List's size should be 3");
-
-        // Проверяем маппинг полей 1-ого элемента списка
-        assertEquals("TEST3", data.get(0).getRegisterType(), "registerType should be 'TEST3'");
-        assertEquals(1000000, data.get(0).getRestIn(), "restIn should be 1000000");
+    @BeforeAll
+    static void setUp() {
+        // Создание таблицы перед запуском тестов
+        DBUtils.createTableIfNotExists("registers");
     }
 
-    @Test
-    void testParseJsonFiles_NegativeScenario_InvalidJsonFormat() throws IOException {
-        // Arrange
-        File resourcesDirectory = Paths.get("src", "test", "resources").toFile();
-        File invalidJsonFile = new File(resourcesDirectory, "Test_invalid.json");
-        try (FileWriter writer = new FileWriter(invalidJsonFile)) {
-            writer.write("This is not a valid JSON format.");
-        }
 
-        // Act and Assert
-        assertThrows(Exception.class, () -> parserJson.parseJsonFiles());
+    @Test
+    void testSelectData() {
+        DBUtils.insertRecords(ParserJson.parseJsonFiles());
+        DBUtils.selectAllRecords().forEach(e -> System.out.println(e.getRegisterType() + " " + e.getRestIn()));
     }
+
 }
